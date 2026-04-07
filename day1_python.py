c0e091a747de2df452cfde86703642d356c5807b
Shruti Gunasekaran <shruti@kovantech.com>
2026-04-07 17:54:47 -0400

    day 1 of learning python

---
+++ day1_python.py
@@ -0,0 +1,23 @@
+# 1. print
+# 2. variables
+# 3. arithmetic 
+# 4. typecasting 
+# 5. input
+# 6. Flow controls
+# 7. logical operators
+# 8.List tuple sets
+#9. functions
+
+captial={"U":1,"A":2}
+
+print(captial)
+
+
+
+
+
+
+    
+
+
+    
---
+++ day22.py
@@ -0,0 +1,34 @@
+''' JavaScript Object Notation '''
+
+import json
+
+# with open('states.json') as f:
+#     data = json.load(f)
+
+# for state in data['states']:
+#     print(state['name'], state['abbreviation'])
+
+
+
+
+
+
+# with open('states.json') as f:
+#     data = json.load(f)
+
+# for state in data['states']:
+#     del state['area_codes']
+
+# with open('new_states.json', 'w') as f:
+#     json.dump(data, f, indent=2)
+
+
+rates
+# from urllib.request import urlopen
+
+# with urlopen("https://open.er-api.com/v6/latest/USD") as response:
+#     source = response.read()
+
+# data = json.loads(source)
+
+# print(json.dumps(data, indent=2))
