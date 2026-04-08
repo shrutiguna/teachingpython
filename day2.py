bac2cf7dfcda66621e9d5923d3936e73c1ce86f0
Shruti Gunasekaran <shruti@kovantech.com>
2026-04-07 21:47:32 -0400

    commite 101

--- day2.py
+++ day2.py
@@ -1,48 +1,16 @@
-''' JavaScript Object Notation '''
-
-import json
-
-people_string = '''
-{
-    "people": [
-        {
-            "name": "John Smith",
-            "phone": "615-555-7164",
-            "emails": ["johnsmith@bogusemail.com", "john.smith@work-place.com"],
-            "has_license": false
-        },
-        {
-            "name": "Jane Doe",
-            "phone": "560-555-5153",
-            "emails": null,
-            "has_license": true
-        }
-    ]
-}
-'''
+# 1. functions
+# 2. Files
+# 3.dictionary
+# 4.json
+# 5. git
 
 
-#print(people_string     )
+file= open("shoppingList.txt","r")
+content= file.read()
+file.close()
 
+items=content.split("\n")
+items.sort()
 
-# to load and manu json data
-# data = json.loads(people_string)
-
-# for person in data['people']:
-#     print(person['name'])
-
-
-#     data = json.loads(people_string)
-
-# for person in data['people']:
-#     del person['phone']
-
-# new_string = json.dumps(data, indent=2, sort_keys=True)
-
-# print(new_string)
-
-# to print them 1 by 1
-# data = json.loads(people_string)
-
-# for person in data['people']:
-#     print(person['name'])
+for item in items:
+    print("-"+item)
--- day22.py
+++ day22.py
@@ -1,37 +1,18 @@
-''' JavaScript Object Notation '''
-
 import json
 
-# with open('states.json') as f:
-#     data = json.load(f)
-
-# for state in data['states']:
-#     print(state['name'], state['abbreviation'])
 
 
+from urllib.request import urlopen
 
+try:
+    with urlopen("https://open.er-api.com/v6/latest/USD") as response:
+        source = response.read()
+    data = json.loads(source)
+    print(json.dumps(data, indent=2))
 
-
-
-# with open('states.json') as f:
-#     data = json.load(f)
-
-# for state in data['states']:
-#     del state['area_codes']
-
-# with open('new_states.json', 'w') as f:
-#     json.dump(data, f, indent=2)
-
-
-#trying to change
-
-# from urllib.request import urlopen
-
-# with urlopen("https://open.er-api.com/v6/latest/USD") as response:
-#     source = response.read()
-
-# data = json.loads(source)
-
-# print(json.dumps(data, indent=2))
+except Exception as e:
+    print(f"Error: {e}")
 
 #new lines added
+
+print("hello world")
